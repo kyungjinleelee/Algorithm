b2ad66d5c5e586b191b4efd786d890e0d5eb331f
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-03-04 23:56:17 +0900

    Update 두 개 뽑아서 더하기.py

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,9 +1,9 @@
 def solution(numbers):
     answer = []
     for i in range(len(numbers)):
-        for j in range(i+1, len(numbers)):
+        for j in range(i+1, len(numbers)):  # i 이후의 인덱스부터 순회
             answer.append(numbers[i] + numbers[j])
-    answerset = set(answer) # 중복 제거
-    answer = list(answerset)
+    answerset = set(answer)     # 중복 제거
+    answer = list(answerset)    # 다시 리스트로 변환
     answer.sort()
     return answer
