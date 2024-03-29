d3eaf63adf83d9746337f10f8e2f1c7662e4d278
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-03-29 16:55:35 +0900

    Update 네트워크(BFS ver).py

--- 프로그래머스/3/43162. 네트워크/네트워크(BFS ver).py
+++ 프로그래머스/3/43162. 네트워크/네트워크(BFS ver).py
@@ -2,15 +2,14 @@ from collections import deque
 
 def bfs(n, computers, com, visited):
     visited[com] = True
-    Q = deque()
-    Q.append(com)
-    while Q:
-        com = Q.popleft()
+    queue = deque()
+    queue.append(com)
+    while queue:
+        com = queue.popleft()
         visited[com] = True 
         for connect in range(n):
-            if connect != com and computers[com][connect] == 1:
-                if visited[connect] == False:
-                    Q.append(connect)
+            if connect != com and computers[com][connect] == 1 and visited[connect] == False:
+                queue.append(connect)
 
 def solution(n, computers):
     answer = 0
