ad1be1cb402e8819a4fbb4965df8907e4f8c08d4
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-03-30 18:19:41 +0900

    Update 의상.py

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -15,15 +15,15 @@ def solution(clothes):
     closet = {}
     
     for name, kind in clothes:
-        if kind in closet.keys():
-            closet[kind] += [name]
+        if kind in closet:
+            closet[kind].append(name)
         else:
             closet[kind] = [name]
             
     # A의 종류가 N개, B의 종류가 M개일 때 가능한 모든 경우의 수 (N+1)(M+1)
     answer = 1
-    for _, value in closet.items():
+    for value in closet.values():
         answer *= (len(value) + 1)
         
     # 아무것도 입지 않은 경우 1을 제외 
-    return answer -1
+    return answer - 1
