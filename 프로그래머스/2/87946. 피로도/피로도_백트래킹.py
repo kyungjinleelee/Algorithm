394084a3f0e513a3a8a4c56c17e889b23b552c8b
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-04-09 20:04:41 +0900

    Update 피로도_백트래킹.py

--- 프로그래머스/2/87946. 피로도/피로도_백트래킹.py
+++ 프로그래머스/2/87946. 피로도/피로도_백트래킹.py
@@ -2,17 +2,19 @@ answer = 0
 N = 0
 visited = []
 
-def dfs(k, cnt, dungeons):
+# dfs 재귀 호출
+def dfs(k, cnt, dungeons): # cnt: 현재까지 방문한 던전의 수
     global answer
     # 최대 방문 횟수 초기화
     if cnt > answer:
         answer = cnt
     
     for i in range(N):
+        # 현재 피로도(k)가 던전의 피로도보다 크고, 방문 안됐을 경우에만 탐색 진행 
         if k >= dungeons[i][0] and not visited[i]:
             # 방문 처리
             visited[i] = 1
-            # 줄어든 피로도로 던전 방문 시작
+            # 피로도 줄이고 다음 던전 방문, 방문 횟수 +1
             dfs(k - dungeons[i][1], cnt + 1, dungeons)
             # 해당 순서로 던전을 모두 방문했으니 다시 초기화
             visited[i] = 0
