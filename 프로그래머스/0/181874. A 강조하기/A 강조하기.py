8cce6162f86381e68ab25332c3a355ce4199509f
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-04-16 20:49:36 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,10 +1,3 @@
 def solution(myString):
-    answer = ''
-    for char in myString:
-        if char == 'a' or char == 'A':
-            answer += 'A'
-        elif char.isupper():
-            answer += char.lower()
-        else:
-            answer += char
-    return answer
+    result = myString.lower()
+    return result.replace('a', 'A')
