9dd906a9c4700133a25c501a19992aca556090e6
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-04-16 20:46:25 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,9 +1,10 @@
 def solution(myString):
-    answer = myString.lower()
-    answer = answer.replace('a', 'A')
+    answer = ''
+    for char in myString:
+        if char == 'a' or char == 'A':
+            answer += 'A'
+        elif char.isupper():
+            answer += char.lower()
+        else:
+            answer += char
     return answer
-
-# 일단 다 소문자로 바꿔 주고, 바꿔준거에서 a를 A로 바꿔주면 됨
-# for문으로 순회할 때보다 오히려 더 쉽게 풀릴 때가 있다. 
-
-        
