d5870b30dbaf7493f6bc166395cd4c3b4d3a0e38
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-04-19 02:09:07 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,9 +1,14 @@
 def solution(num_list):
-    for num in num_list:
-        if num_list[-1] > num_list[-2]:
-            num_list.append(num_list[-1] - num_list[-2])
-            break
-        else:
-            num_list.append(num_list[-1] * 2)
-            break
+    a = num_list[-1]
+    b = num_list[-2]
+    if a > b:
+        num_list.append(a - b)  
+    else:
+        num_list.append(a * 2)        
     return num_list
+
+'''
+주의할 것 
+1. 무조건 for문 돌아야 한다고 생각하지 말 것 
+2. 길거나 가독성을 해칠 만한 조건이 있으면 변수에 저장해서 쓸 것
+'''
