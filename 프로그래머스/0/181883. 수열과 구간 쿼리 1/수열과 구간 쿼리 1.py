85f1f56df4c8d7bcef2a44e10c7d36f11f71b533
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-05-01 10:14:02 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 13.76 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -1,7 +1,7 @@
 def solution(arr, queries):
     answer = []
-    for i in range(len(arr)):
-        for j in range(len(queries)):
-            if queries[j][0] <= i and i <= queries[j][1]:
-                arr[i] += 1
+    for query in queries:
+        s, e = query
+        for i in range(s, e+1):
+            arr[i] += 1
     return arr
