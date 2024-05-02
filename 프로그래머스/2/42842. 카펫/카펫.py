46a54a8e2cfabda47405dd3f98ca64513a0c7565
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-05-02 09:47:27 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,32 +1,15 @@
 def solution(brown, yellow):
     answer = []
-    # 총 블록 수 (a * b = total)
+    # 총 블록 수 
     total = brown + yellow
     
     # a: 가로의 길이
     # b: 세로의 길이 
     for b in range(1, total + 1):
-        if (total / b) % 1 == 0:    # total / b = a
+        if (total / b) % 1 == 0:
             a = total / b
             if a >= b:
                 if 2*a + 2*b == brown + 4:
                     return [a, b]
 
     return answer 
-
-
-"""
-<조건>
-a = 가로의 길이
-b = 세로의 길이 
-
-1. a >= b
-2. 2a + 2b - 4 = brown
-3. (a-2) * (b-2) = yellow
-↓
-2. 2a + 2b = brown + 4
-3. ab - 2a - 2b + 4 = yellow
-
--> ab - brown - 4 + 4 = yellow
--> ab = yellow + brown
-"""
