e38100182dcf99ab9e2a6f5f713d54d696cd3174
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-05-31 10:32:23 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    even_cnt = 0
+    odd_cnt = 0
+    
+    for num in num_list:
+        if num % 2 == 0:
+            even_cnt += 1
+        else:
+            odd_cnt += 1
+    return [even_cnt, odd_cnt]
