7a40ad0fa2147164b1be12799e2fe976f98cb606
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2024-12-13 02:43:19 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 311.90 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,47 +1,39 @@
 from collections import deque
 
 def solution(land):
-    answer = 0
-    n, m = len(land), len(land[0])
-    
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    # 각 열의 기름 총량
-    result = [0 for i in range(m)]
-    # 방문 기록
-    visited = [[0 for _ in range(m)] for _ in range(n)]
-    
-    def bfs(pos):
-        a, b = pos
-        cnt = 1
-        visited[a][b] = 1
+    n = len(land)
+    m = len(land[0])
+    visited = [[False] * m for _ in range(n)]  # 방문 처리를 위한 2차원 배열
 
-        q = deque()
-        q.append(pos)
-        
-        # 석유가 존재하는 열 저장 (중복 방지)
-        oil_covered = set()
-        oil_covered.add(b)
-        
-        while q:
-            x, y = q.popleft()
-            for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
-                if (0 <= nx < n) and (0 <= ny < m) and visited[nx][ny] == 0 and land[nx][ny] == 1:
-                    q.append((nx, ny))
-                    visited[nx][ny] = 1
-                    cnt += 1
-                    oil_covered.add(ny)
-                    
-        for c in oil_covered:
-            result[c] += cnt
-            
-    for i in range(n):
-        for j in range(m):
-            if (land[i][j] == 1) and not visited[i][j]:
-                bfs((i, j))
-    
-    answer = max(result)
-    
-    return answer
+    # 현재 위치에서 상하좌우로 연결된 기름 덩어리들을 탐색하는 bfs
+    def bfs(x, y):
+        queue = deque([(x, y)])
+        visited[x][y] = True
+        size = 1  # 초기화
+        while queue:
+            cur_x, cur_y = queue.popleft()
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx, ny = cur_x + dx, cur_y + dy
+                # 현재 x, y좌표가 유효 범위에 있는지, 방문했는지 여부 검사
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                if visited[nx][ny] or land[nx][ny] == 0:
+                    continue
+                # 방문 표시 및 연결된 석유 카운트
+                visited[nx][ny] = True
+                size += 1
+                queue.append((nx, ny))
+        return size
+
+    max_oil = 0
+    # 모든 석유 덩어리 크기 계산 (석유가 있고 방문하지 않았으면 bfs 호출)
+    for col in range(m):
+        current_oil = 0
+        visited = [[False] * m for _ in range(n)]
+        for row in range(n):
+            if land[row][col] == 1 and not visited[row][col]:
+                # 새로운 덩어리 발견
+                chunk_size = bfs(row, col)
+                current_oil += chunk_size
+        max_oil = max(max_oil, current_oil)
+    return max_oil
