96b4de5e87ed41a065cf56f3f575f791c7bbd94a
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2025-08-04 15:45:07 +0900

    [level 1] Title: 추억 점수, Time: 1.30 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,16 +1,12 @@
 def solution(name, yearning, photo):
-    answer = []
-    # 이름과 점수를 매핑하는 딕셔너리
-    score = dict(zip(name, yearning))
+    result = []
+    # 이름 별 그리움 점수 매핑
+    score = {n: y for n, y in zip(name, yearning)} # {'may': 5, 'kein': 10, 'kain': 1, 'radi': 3}
     
-    # 1. photo의 각 리스트와 그 원소들을 순회
-    for i in range(len(photo)):
-        # 2. 총 그리움 점수를 초기화하는 변수
+    for p in photo:
         total = 0
-        for j in range(len(photo[i])):
-            # 3. 만약 해당 이름이 그리워하는 사람이라면 그리움 점수 추가
-            if photo[i][j] in name:
-                total += score[photo[i][j]]
-        # 4. 정답 배열에 총 그리움 점수를 추가
-        answer.append(total)
-    return answer
+        for i in p:
+            if i in score:
+                total += score[i]
+        result.append(total)
+    return result
