7f384deb163d2f87e6933b141596b1c903371844
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2025-09-04 16:55:02 +0900

    [level 2] Title: 전화번호 목록, Time: 99.76 ms, Memory: 29.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,9 @@
 def solution(phone_book):
-    answer = True
+    # 접두어 검사 누락을 막기 위해 정렬
+    # 또한 정렬 시 접두어 검사를 효율적으로 할 수 있음
     phone_book.sort()
     
-    for i in range(len(phone_book) - 1):
-        if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
-            answer = False
-            break
-    return answer
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    return True
