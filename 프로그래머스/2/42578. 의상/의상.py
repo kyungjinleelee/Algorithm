4bf2e143eb5b7cdf5aa091ed096ba35a6eb20000
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2025-09-05 15:13:33 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,29 +1,15 @@
-"""
-문제에서 최소 1개의 의상을 입어야 한다고 했으므로, 
-경우에 따라 어떤 종류는 착용하지 않을 수도 있다는 점을 생각하자
-<경우의 수>
-(N+1)(M+1) = NM + N + M + 1
-
-NM: N과 M을 모두 사용하는 경우
-N: N만 사용하는 경우
-M: M만 사용하는 경우
-1: 모두 사용하지 않는 경우 
-"""
 def solution(clothes):
-    # 각 종류 별 가진 의상을 저장 (종류:[이름, 이름, ...])
-    # 종류를 key로 하는 딕셔너리 만듦
-    closet = {}
-    
-    for name, kind in clothes:
-        if kind in closet:
-            closet[kind].append(name)
+    dict = {}
+    for value, key in clothes:
+        if key in dict:
+            dict[key].append(value)
         else:
-            closet[kind] = [name]
-            
-    # A의 종류가 N개, B의 종류가 M개일 때 가능한 모든 경우의 수 (N+1)(M+1)
+            dict[key] = [value]
+
     answer = 1
-    for value in closet.values():
-        answer *= (len(value) + 1)
+    for key in dict:
+        answer *= len(dict[key]) + 1
         
-    # 아무것도 입지 않은 경우 1을 제외 
-    return answer - 1
+    # 아무것도 안 입는 경우는 제외하기 위해 -1
+    answer -= 1
+    return answer
